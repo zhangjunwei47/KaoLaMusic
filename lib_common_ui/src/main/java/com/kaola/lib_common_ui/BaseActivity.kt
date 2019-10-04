841e4f10540f0959e2fae3596bb9f4b0276fcdaf
zhangchao <zhangchao1@kaolafm.com>
2019-10-04 08:11:59 +0800

    添加沉浸式

--- lib_common_ui/src/main/java/com/kaola/lib_common_ui/BaseActivity.kt
+++ lib_common_ui/src/main/java/com/kaola/lib_common_ui/BaseActivity.kt
@@ -2,11 +2,13 @@ package com.kaola.lib_common_ui
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import com.kaola.lib_common_ui.util.StatusBarUtil
+
 open class BaseActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        StatusBarUtil.statusBarLightMode(this)
     }
 
 }
---
+++ lib_common_ui/src/main/java/com/kaola/lib_common_ui/util/StatusBarUtil.kt
@@ -0,0 +1,225 @@
+package com.kaola.lib_common_ui.util
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import android.util.DisplayMetrics
+import android.view.Display
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import java.lang.reflect.Field
+import java.lang.reflect.Method
+
+/**
+ * @author renzhiqiang
+ * @date 2019/4/24
+ */
+object StatusBarUtil {
+    /**
+     * 修改状态栏为全透明
+     */
+    fun transparencyBar(activity: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = activity.window
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = Color.TRANSPARENT
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val window = activity.window
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+            )
+        }
+    }
+
+    /**
+     * 状态栏亮色模式，设置状态栏黑色文字、图标，
+     * 适配4.4以上版本MIUIV、Flyme和6.0以上版本其他Android
+     *
+     * @return 1:MIUUI 2:Flyme 3:android6.0
+     */
+    fun statusBarLightMode(activity: Activity) {
+        var result = 0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (miuiSetStatusBarLightMode(activity, true)) {
+                result = 1
+            } else if (flymeSetStatusBarLightMode(activity.window, true)) {
+                result = 2
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                activity.window
+                    .decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                result = 3
+            }
+        }
+    }
+
+    /**
+     * 已知系统类型时，设置状态栏黑色文字、图标。
+     * 适配4.4以上版本MIUIV、Flyme和6.0以上版本其他Android
+     *
+     * @param type 1:MIUUI 2:Flyme 3:android6.0
+     */
+    fun statusBarLightMode(activity: Activity, type: Int) {
+        if (type == 1) {
+            miuiSetStatusBarLightMode(activity, true)
+        } else if (type == 2) {
+            flymeSetStatusBarLightMode(activity.window, true)
+        } else if (type == 3) {
+            activity.window
+                .decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+    }
+
+    /**
+     * 状态栏暗色模式，清除MIUI、flyme或6.0以上版本状态栏黑色文字、图标
+     */
+    fun StatusBarDarkMode(activity: Activity, type: Int) {
+        if (type == 1) {
+            miuiSetStatusBarLightMode(activity, false)
+        } else if (type == 2) {
+            flymeSetStatusBarLightMode(activity.window, false)
+        } else if (type == 3) {
+            activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+        }
+    }
+
+    /**
+     * 设置状态栏图标为深色和魅族特定的文字风格
+     * 可以用来判断是否为Flyme用户
+     *
+     * @param window 需要设置的窗口
+     * @param dark 是否把状态栏文字及图标颜色设置为深色
+     * @return boolean 成功执行返回true
+     */
+    fun flymeSetStatusBarLightMode(window: Window?, dark: Boolean): Boolean {
+        var result = false
+        if (window != null) {
+            try {
+                val lp = window.attributes
+                val darkFlag =
+                    WindowManager.LayoutParams::class.java.getDeclaredField("MEIZU_FLAG_DARK_STATUS_BAR_ICON")
+                val meizuFlags =
+                    WindowManager.LayoutParams::class.java.getDeclaredField("meizuFlags")
+                darkFlag.isAccessible = true
+                meizuFlags.isAccessible = true
+                val bit = darkFlag.getInt(null)
+                var value = meizuFlags.getInt(lp)
+                if (dark) {
+                    value = value or bit
+                } else {
+                    value = value and bit.inv()
+                }
+                meizuFlags.setInt(lp, value)
+                window.attributes = lp
+                result = true
+            } catch (e: Exception) {
+
+            }
+
+        }
+        return result
+    }
+
+    /**
+     * 需要MIUIV6以上
+     *
+     * @param dark 是否把状态栏文字及图标颜色设置为深色
+     * @return boolean 成功执行返回true
+     */
+    fun miuiSetStatusBarLightMode(activity: Activity, dark: Boolean): Boolean {
+        var result = false
+        val window = activity.window
+        if (window != null) {
+            val clazz = window.javaClass
+            try {
+                val darkModeFlag: Int
+                val layoutParams = Class.forName("android.view.MiuiWindowManager\$LayoutParams")
+                val field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_DARK_MODE")
+                darkModeFlag = field.getInt(layoutParams)
+
+                val extraFlagField = clazz.getMethod(
+                    "setExtraFlags",
+                    Int::class.javaPrimitiveType,
+                    Int::class.javaPrimitiveType
+                )
+                if (dark) {
+                    //状态栏透明且黑色字体
+                    extraFlagField.invoke(window, darkModeFlag, darkModeFlag)
+                } else {
+                    //清除黑色字体
+                    extraFlagField.invoke(window, 0, darkModeFlag)
+                }
+                result = true
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    //开发版 7.7.13 及以后版本采用了系统API，旧方法无效但不会报错，所以两个方式都要加上
+                    if (dark) {
+                        activity.window
+                            .decorView.systemUiVisibility =
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                    } else {
+                        activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+                    }
+                }
+            } catch (e: Exception) {
+
+            }
+
+        }
+        return result
+    }
+
+    //获取屏幕虚拟键高度
+    fun getVirtualBarHeight(context: Context): Int {
+        var vh = 0
+        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display = windowManager.defaultDisplay
+        val dm = DisplayMetrics()
+        try {
+            val c = Class.forName("android.view.Display")
+            val method = c.getMethod("getRealMetrics", DisplayMetrics::class.java)
+            method.invoke(display, dm)
+            vh = dm.heightPixels - display.height
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return vh
+    }
+
+    fun getStatusBarHeight(context: Context): Int {
+        var height = 0
+        try {
+            val c = Class.forName("com.android.internal.R\$dimen")
+            val f = c.getField("status_bar_height")
+            val id = f.get(null) as Int
+            height = context.resources.getDimensionPixelSize(id)
+        } catch (e: Exception) {
+        }
+
+        return height
+    }
+
+    fun dip2px(context: Context, dpValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (dpValue * scale + 0.5f).toInt()
+    }
+
+    fun px2dip(context: Context, pxValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (pxValue / scale + 0.5f).toInt()
+    }
+
+    /** 获取手机的密度  */
+    fun getDensity(context: Context): Float {
+        val dm = context.resources.displayMetrics
+        return dm.density
+    }
+}

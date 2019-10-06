d6704ecd3559fa04f8f55c5265c7376921838b43
zhangchao <zhangchao1@kaolafm.com>
2019-10-06 15:41:33 +0800

    添加 网络

--- lib_network/src/main/java/com/kaola/lib_network/RetrofitFactory.kt
+++ lib_network/src/main/java/com/kaola/lib_network/RetrofitFactory.kt
@@ -5,6 +5,7 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
@@ -29,7 +30,7 @@ class RetrofitFactory private constructor() {
         mRetrofit = Retrofit.Builder()
             .baseUrl(NetworkConstant.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            //.addCallAdapterFactory()
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
             .client(initClient())
             .build()
     }
---
+++ lib_network/src/main/java/com/kaola/lib_network/api/HomeApi.kt
@@ -0,0 +1,6 @@
+package com.kaola.lib_network.api
+
+interface HomeApi
+{
+    
+}
